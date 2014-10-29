7259397e1c8f69ddbbfbf5cc8c8b8d1503557e82
rizar <dimabv@tut.by>
2014-10-29 17:28:42 +0100

    SequenceGenerator works

--- blocks/sequence_generators.py
+++ blocks/sequence_generators.py
@@ -1,32 +1,73 @@
-from theano import tensor
+import copy
 from functools import partial
-from blocks.bricks import Brick, MLP, BaseRecurrent, Identity, ForkInputs
+from theano import tensor
+from abc import abstractmethod
+from blocks.bricks import (
+    Brick, MLP, BaseRecurrent, Identity, ForkInputs, ApplySignature,
+    zero_state)
+
+
+class TrivialEmitter(Brick):
+
+    def __init__(self, output_dim, **kwargs):
+        super(TrivialEmitter, self).__init__(**kwargs)
+        self.output_dim = output_dim
+
+    @Brick.apply_method
+    def emit(self, summaries):
+        return summaries
+
+    @emit.signature_method
+    def emit_signature(self, *args, **kwargs):
+        return ApplySignature(output_dims=[self.output_dim])
+
+    @abstractmethod
+    def cost(self, summaries, outputs):
+        pass
+
+
+class TrivialFeedback(Brick):
+
+    @Brick.lazy_method
+    def __init__(self, output_dim, **kwargs):
+        super(TrivialFeedback, self).__init__(**kwargs)
+        self.output_dim = output_dim
+
+    @Brick.apply_method
+    def apply(self, outputs):
+        return outputs
+
+    @apply.signature_method
+    def apply_signature(self):
+        return ApplySignature(output_dims=[self.output_dim])
 
 
 class NullStateInitializer(Brick):
 
     def __init__(self, state_names, context_names, null_output, **kwargs):
-        super(Brick, self).__init__(**kwargs)
+        super(NullStateInitializer, self).__init__(**kwargs)
         self.__dict__.update(**locals())
 
         assert len(self.context_names) == 0
 
+    @Brick.apply_method
     def initialize_state(self, state_name, batch_size, **kwargs):
-        state_index = self.state_names.index(state_name)
         if state_name == 'outputs':
-            pattern = (batch_size,) + (1,) * self.null_output.ndim
-            return tensor.tile(pattern)
+            shape = [batch_size] + [self.null_output.shape[i]
+                                    for i in range(self.null_output.ndim)]
+            return tensor.alloc(self.null_output, *shape)
         else:
-            return BaseRecurrent.zero_state(
+            state_index = self.state_names.index(state_name)
+            return zero_state(
                 None, self.state_dims[state_index], batch_size)
 
 
 class DefaultSummarizer(Brick):
 
-    @Brick.lazy_init
-    def __init__(self, state_names, context_names, summary_dim,
+    @Brick.lazy_method
+    def __init__(self, state_names, context_names,
                  weights_init, biases_init, **kwargs):
-        super(Brick, self).__init__(**kwargs)
+        super(DefaultSummarizer, self).__init__(**kwargs)
         self.__dict__.update(**locals())
 
         self.input_names = ['feedback'] + state_names + context_names
@@ -36,7 +77,7 @@ class DefaultSummarizer(Brick):
         self.children = self.projectors
 
     def _push_allocation_config(self):
-        self.dims = self.state_dims + self.context_dims
+        self.dims = [self.feedback_dim] + self.state_dims + self.context_dims
         for projector, dim in zip(self.projectors, self.dims):
             projector.dims[0] = dim
             projector.dims[-1] = self.summary_dim
@@ -50,15 +91,11 @@ class DefaultSummarizer(Brick):
 
     @Brick.apply_method
     def summarize(self, **kwargs):
-        projections = [projector(kwargs[name]) for name, projector in
+        projections = [projector.apply(kwargs[name]) for name, projector in
                        zip(self.input_names, self.projectors)]
         if len(projections) == 1:
             return projections[0]
         return sum(projections[1:], projections[0])
-
-
-class AddContexts(BaseRecurrent):
-    pass
 
 
 def dict_union(*dicts, **kwargs):
@@ -69,7 +106,7 @@ def dict_union(*dicts, **kwargs):
     return result
 
 
-class SequenceGenerator(BaseRecurrent):
+class BaseSequenceGenerator(BaseRecurrent):
     """A context-dependent sequence generator.
 
     This brick can generate a sequence by the following algorithm:
@@ -80,12 +117,11 @@ class SequenceGenerator(BaseRecurrent):
     2. The current state, the previous output and the contexts are
     summarized in a single varible using the `summarizer` brick.
 
-    3. An output is generated from the summary by the `generator` brick.
+    3. An output is generated from the summary by the `emitter` brick.
 
     4. The next state is obtained as follows. The feedback produced from the
-    output by the `feedback` brick is fed into the `transition` brick
-    wrapped by `fork_inputs` and `add_contexts` blocks. The wrappers ensure
-    that the transition function has one input and uses contexts.
+    output by the `feedback` brick is fed into the `transition`. It is assumed
+    that `transition` has only one input.
 
     5. Continue from step 2 while the number of steps done is less then
     required.
@@ -95,97 +131,134 @@ class SequenceGenerator(BaseRecurrent):
     procedure.
 
     """
-    @Brick.lazy_init
-    def __init__(self, dim, summary_dim, context_names, context_dims,
-                 transition, generator, feedback, null_output,
+    @Brick.lazy_method
+    def __init__(self, transition, emitter, feedback, null_output,
+                 initializer=None, summarizer=None,
                  weights_init=None, biases_init=None, **kwargs):
-        super(SequenceGenerator, self).__init__(**kwargs)
+        super(BaseSequenceGenerator, self).__init__(**kwargs)
         self.__dict__.update(**locals())
         del self.self
 
-        # Wrap the transition to ensure that is one input and uses contexts
-        self.add_contexts = AddContexts(transition, context_names)
-        self.fork_inputs = ForkInputs(self.add_contexts)
-        self.wrapped_transition = self.fork_inputs
+        self.transition_signature_func = (
+            self.transition.get_signature_func('apply'))
+        signature = self.transition_signature_func()
+        assert len(signature.input_names) == 2
+        assert signature.num_outputs == 0
+        self.state_names = signature.state_names
+        self.context_names = signature.context_names
 
-        # Get the transition state names
-        self.wrapped_signature_func = partial(getattr(
-            self.wrapped_transition.__class__, 'apply').signature, self)
-        signature = self.wrapped_signature_func()
-        self.states_names = signature.state_names
+        if not self.initializer:
+            self.initializer = NullStateInitializer(
+                self.state_names, self.context_names, null_output)
+        if not self.summarizer:
+            self.summarizer = DefaultSummarizer(
+                self.state_names, self.context_names)
 
-        self.initializer = NullStateInitializer(self.state_names,
-                                                context_names, null_output)
-        self.summarizer = DefaultSummarizer(self.state_names,
-                                            context_names)
-
-        self.children = [self.wrapped_transition, self.initializer,
-                         self.summarizer, self.generator, self.feedback]
+        self.children = [self.transition, self.initializer,
+                         self.summarizer, self.emitter, self.feedback]
 
     def _push_allocation_config(self):
-        self.add_contexts.context_dims = self.context_dims
-        self.fork_inputs.input_dim = self.dim
+        transition_signature = self.transition_signature_func()
+        state_dims = [transition_signature.dims[name]
+                      for name in transition_signature.state_names]
+        context_dims = [transition_signature.dims[name]
+                        for name in transition_signature.context_names]
 
-        self.transition.dim = self.dim
-        signature = self.wrapped_signature_func()
-        state_dims = {name: signature.dims[name]
-                      for name in signature.state_names}
+        feedback_signature = self.feedback.get_signature_func('apply')()
+        self.summarizer.feedback_dim = feedback_signature.output_dims[0]
 
         for brick in self.summarizer, self.initializer:
             brick.state_dims = state_dims
-            brick.context_dims = self.context_dims
+            brick.context_dims = context_dims
 
     def _push_initialization_config(self):
         # Does not touch `transition` initilization
-        for brick in [self.add_contexts, self.fork_inputs, self.initializer,
-                      self.summarizer, self.generator, self.feedback]:
+        for brick in [self.initializer, self.summarizer, self.feedback]:
             if self.weights_init:
                 brick.weights_init = self.weights_init
             if self.biases_init:
                 brick.biases_init = self.weights_init
 
-    def cost(self, outputs, mask, **kwargs):
+    @Brick.apply_method
+    def cost(self, outputs, mask=None, **kwargs):
         batch_size = outputs.shape[-2]
 
-        contexts = [kwargs[name] for name in self.context_names]
+        contexts = {name : kwargs[name] for name in self.context_names}
         initial_states = {name: self.initializer.initialize_state(
                             name, batch_size=batch_size, **contexts)
                           for name in self.state_names}
 
         feedback = self.feedback.apply(outputs)
-        states = self.wrapped_transition.apply(
-            feedback, iterate=True, return_initial_states=True,
-            **dict_union(initial_states, contexts))
+        states = [state[:-1] for state in self.transition.apply(
+            feedback, mask=mask, iterate=True,
+            return_initial_states=True, return_list=True,
+            **dict_union(initial_states, contexts))]
         states = dict(zip(self.state_names, states))
 
         feedback = tensor.roll(feedback, 1, 0)
         feedback = tensor.set_subtensor(feedback[0],
             self.feedback.apply(self.initializer.initialize_state(
                 'outputs', batch_size=batch_size, **contexts)))
-        summaries = self.summarizer(feedback=feedback,
-                                    **dict_union(states, contexts))
-        return self.generator.cost(summaries[:-1], outputs)
+        summaries = self.summarizer.summarize(
+            feedback=feedback, **dict_union(states, contexts))
+        return self.emitter.cost(summaries, outputs)
 
     @BaseRecurrent.recurrent_apply_method
     def generate(self, outputs, **kwargs):
-        states = [kwargs[name] for name in self.state_names]
-        contexts = [kwargs[name] for name in self.context_names]
-        summaries = self.summarizer(feedback=self.feedback.apply(outputs),
-                                    **dict_union(states, contexts))
-        next_outputs = self.generator.generate(summaries)
-        next_costs = self.generator.cost(summaries, next_outputs)
-        next_states = self.wrapped_transition.apply(
+        states = {name: kwargs[name] for name in self.state_names}
+        contexts = {name: kwargs[name] for name in self.context_names}
+        summaries = self.summarizer.summarize(
+            feedback=self.feedback.apply(outputs),
+            **dict_union(states, contexts))
+        next_outputs = self.emitter.emit(summaries)
+        next_costs = self.emitter.cost(summaries, next_outputs)
+        next_states = self.transition.apply(
             self.feedback.apply(next_outputs), **dict_union(states, contexts))
         return next_states, next_outputs, next_costs
 
     @generate.signature_method
-    def generate_signature(self):
-        signature = self.wrapped_signature_func()
+    def generate_signature(self, *args, **kwargs):
+        signature = copy.deepcopy(self.transition_signature_func())
         signature.state_names.append('outputs')
+        signature.dims['outputs'] = (
+            self.emitter.get_signature_func('emit')().output_dims[0])
+        signature.num_outputs = 1
         for state_name in signature.state_names:
-            def call_initializer(self, batch_size, *args, **kwargs):
-                contexts = [kwargs[name] for name in self.context_names]
+            # Adapt `self.initializer` to the standard
+            # state initialization function interface
+            def call_initializer(state_name, self, dim, batch_size,
+                                 *args, **kwargs):
+                contexts = {name: kwargs[name] for name in self.context_names}
                 return self.initializer.initialize_state(
                     state_name, batch_size=batch_size, **contexts)
-            signature.state_init_func[state_name] = call_initializer
+            signature.state_init_funcs[state_name] = (
+                partial(call_initializer, state_name))
         return signature
+
+
+class SequenceGenerator(BaseSequenceGenerator):
+    """A more user-friendly subclass of the BaseSequenceGenerator.
+
+    This class is responsible for wrapping the transition and adding the
+    necessary contexts.
+
+    .. todo:: context addition
+
+    """
+
+    def __init__(self, transition, *args, **kwargs):
+        self.fork_inputs_wrapper = ForkInputs(transition)
+        super(SequenceGenerator, self).__init__(self.fork_inputs_wrapper,
+                                                *args, **kwargs)
+
+    def _push_allocation_config(self):
+        feedback_signature = self.feedback.get_signature_func('apply')()
+        self.fork_inputs_wrapper.input_dim = feedback_signature.output_dims[0]
+        super(SequenceGenerator, self)._push_allocation_config()
+
+    def _push_initialization_config(self):
+        if self.weights_init:
+            self.fork_inputs_wrapper.weights_init = self.weights_init
+        if self.biases_init:
+            self.fork_inputs_wrapper.biases_init = self.biases_init
+        super(SequenceGenerator, self)._push_initialization_config()
---
+++ blocks/tests/test_seqgen.py
@@ -0,0 +1,53 @@
+import numpy
+
+import theano
+from theano import tensor
+
+from blocks.bricks import Brick, GatedRecurrent, Tanh
+from blocks.sequence_generators import (
+    SequenceGenerator, TrivialEmitter, TrivialFeedback)
+from blocks.initialization import Orthogonal, IsotropicGaussian, Constant
+
+floatX = theano.config.floatX
+
+class MSEEmitter(TrivialEmitter):
+
+    @Brick.apply_method
+    def cost(self, summaries, outputs):
+        return ((summaries - outputs) ** 2).sum(axis=summaries.ndim - 1)
+
+
+def test_sequence_generator():
+    output_dim = 1
+    dim = 20
+    batch_size = 30
+    n_steps = 10
+
+    transition = GatedRecurrent(
+        name="transition", activation=Tanh(), dim=dim,
+        weights_init=Orthogonal())
+
+    generator = SequenceGenerator(
+        transition, MSEEmitter(output_dim, name="emitter"),
+        TrivialFeedback(output_dim, name="feedback"),
+        null_output=tensor.zeros((output_dim,)),
+        weights_init=IsotropicGaussian(0.01), biases_init=Constant(0))
+    generator.summarizer.summary_dim = output_dim
+
+    y = tensor.tensor3('y')
+    mask = tensor.matrix('mask')
+    costs = generator.cost(y, mask)
+    assert costs.ndim == 2
+    costs_val = theano.function([y, mask], [costs])(
+        numpy.zeros((n_steps, batch_size, output_dim), dtype=floatX),
+        numpy.ones((n_steps, batch_size), dtype=floatX))[0]
+    assert costs_val.shape == (n_steps, batch_size)
+
+    states, outputs, costs = [variable.eval() for variable in
+                              generator.generate(
+                                  iterate=True, batch_size=batch_size,
+                                  n_steps=n_steps)]
+    assert states.shape == (n_steps, batch_size, dim)
+    assert outputs.shape == (n_steps, batch_size, output_dim)
+    assert costs.shape == (n_steps, batch_size)
+
