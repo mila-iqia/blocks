b0723c89b0923e76d6f55d425656cbaa11f2ad24
Laurent Dinh <dinh.laurent@gmail.com>
2014-12-02 21:23:33 -0500

    Attention correction

--- blocks/tests/test_attention.py
+++ blocks/tests/test_attention.py
@@ -10,6 +10,7 @@ floatX = theano.config.floatX
 
 def test_sequence_content_attention():
     # Disclaimer: only check dimensions, not values
+    rng = numpy.random.RandomState([2014, 12, 2])
 
     seq_len = 5
     batch_size = 6
@@ -33,6 +34,9 @@ def test_sequence_content_attention():
     seq_values = numpy.zeros((seq_len, batch_size, sequence_dim), dtype=floatX)
     states_values = numpy.zeros((batch_size, state_dim), dtype=floatX)
     mask_values = numpy.zeros((seq_len, batch_size), dtype=floatX)
+    # randomly generate a sensible mask
+    for sed_idx in range(batch_size):
+        mask_values[:rng.randint(1, seq_len), sed_idx] = 1
     glimpses_values, weight_values = theano.function(
         [sequences, states, mask], [glimpses, weights])(
             seq_values, states_values, mask_values)
@@ -40,3 +44,6 @@ def test_sequence_content_attention():
     assert weight_values.shape == (batch_size, seq_len)
     assert numpy.all(weight_values >= 0)
     assert numpy.all(weight_values <= 1)
+    assert numpy.all(weight_values.sum(axis=1) == 1)
+    assert numpy.all((weight_values.T == 0) == (mask_values == 0))
+
