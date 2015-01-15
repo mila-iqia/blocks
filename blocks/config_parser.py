be10f9d3867ce3ddd3d1bf3b4ccaf4c97de6a004
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-15 09:42:51 -0500

    Config tests

--- blocks/config_parser.py
+++ blocks/config_parser.py
@@ -79,10 +79,7 @@ class Configuration(object):
         if value is NOT_SET:
             raise ConfigurationError("Configuration not set and no default "
                                      "provided: {}.".format(key))
-        if type is None:
-            return value
-        else:
-            return config['type'](value)
+        return config['type'](value)
 
     def add_config(self, key, type, default=NOT_SET, env_var=None):
         """Add a configuration setting.
