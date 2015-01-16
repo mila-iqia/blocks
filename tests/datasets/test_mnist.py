1cf439f108e9f19ef36ca0c2de48ea902314afbe
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-16 12:44:28 -0500

    Fix MNIST test

--- tests/datasets/test_mnist.py
+++ tests/datasets/test_mnist.py
@@ -5,11 +5,11 @@ from blocks.datasets.mnist import MNIST
 
 def test_mnist():
     mnist_train = MNIST('train', start=20000)
-    assert len(mnist_train.X) == 40000
-    assert len(mnist_train.y) == 40000
+    assert len(mnist_train.data['features']) == 40000
+    assert len(mnist_train.data['targets']) == 40000
     mnist_test = MNIST('test', sources=('targets',))
-    assert len(mnist_test.X) == 10000
-    assert len(mnist_test.y) == 10000
+    assert len(mnist_test.data['features']) == 10000
+    assert len(mnist_test.data['targets']) == 10000
 
     first_feature, first_target = mnist_train.get_data(request=[0])
     assert first_feature.shape == (1, 784)
