32991e16050bc0cdd830a01b064dc324ac600ce3
rizar <dimabv@tut.by>
2015-01-16 10:59:06 +0100

    BinaryCrossEntropy to work with indices direcly

--- blocks/bricks/cost.py
+++ blocks/bricks/cost.py
@@ -25,6 +25,15 @@ class BinaryCrossEntropy(CostMatrix):
         cost = tensor.nnet.binary_crossentropy(y_hat, y)
         return cost
 
+    @application
+    def apply_for_indices(self, y_ones_indices, y_hat):
+        flat_ones_indices = y_ones_indices.flatten()
+        return -(tensor.log(
+            y_hat.flatten()
+                [y_hat.shape[-1]
+                 * tensor.arange(flat_ones_indices.shape[0])
+                 + flat_ones_indices])).mean()
+
 
 class AbsoluteError(CostMatrix):
     @application
