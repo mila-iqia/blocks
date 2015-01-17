f4901e7a142785db3e80d25f71345bcf2e7b2063
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-17 14:14:44 -0500

    Use nose2 fixtures to silence training progress of examples tests; note that Blocks still uses printing right now, but that should change

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,19 +1,32 @@
 import logging
 import os
 
-from examples.mnist import main as mnist
-from examples.pylearn2.markov_chain import main as pylearn2
+import pylearn2
+
+import blocks
+from examples.mnist import main as mnist_test
+from examples.pylearn2.markov_chain import main as pylearn2_test
 
 
-def test_mnist():
-    mnist()
-
-
-def test_pylearn2():
+def setup():
     # Silence Pylearn2's logger
     logger = logging.getLogger(pylearn2.__name__)
     logger.setLevel(logging.ERROR)
 
+    # Silence Block's logger
+    logger = logging.getLogger(blocks.__name__)
+    logger.setLevel(logging.ERROR)
+
+
+def test_mnist():
+    mnist_test()
+
+test_mnist.setup = setup
+
+
+def test_pylearn2():
     filename = 'unittest_markov_chain'
-    pylearn2('train', filename, 0, 3, False)
+    pylearn2_test('train', filename, 0, 3, False)
     os.remove(filename)
+
+test_pylearn2.setup = setup
