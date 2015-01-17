248ac2a63ad096cb6975c1faeadb012ee0c2c47f
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-17 13:06:51 -0500

    Move pylearn2 example to test_examples.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,5 +1,19 @@
+import logging
+import os
+
 from examples.mnist import main as mnist
+from examples.pylearn2.markov_chain import main as pylearn2
 
 
 def test_mnist():
     mnist()
+
+
+def test_pylearn2():
+    # Silence Pylearn2's logger
+    logger = logging.getLogger(pylearn2.__name__)
+    logger.setLevel(logging.ERROR)
+
+    filename = 'unittest_markov_chain'
+    pylearn2('train', filename, 0, 3, False)
+    os.remove(filename)
