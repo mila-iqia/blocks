ef926e05263015edffbf7edc9c4fbfadf7a6f8a6
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-18 11:48:45 -0500

    Cleanup the test files

--- tests/datasets/test_serialization.py
+++ tests/datasets/test_serialization.py
@@ -17,16 +17,21 @@ def test_in_memory():
     for i, (features, targets) in enumerate(epoch):
         if i == 1:
             break
-    numpy.all(features == mnist.data['features'][256:512])
+    assert numpy.all(features == mnist.data['features'][256:512])
 
     # Pickle the epoch and make sure that the data wasn't dumped
-    with open('epoch_test.pkl', 'wb') as f:
+    filename = 'epoch_test.pkl'
+    assert not os.path.exists(filename)
+    with open(filename, 'wb') as f:
         dill.dump(epoch, f, protocol=dill.HIGHEST_PROTOCOL)
-    assert os.path.getsize('epoch_test.pkl') < 1024 * 1024  # Less than 1MB
+    assert os.path.getsize(filename) < 1024 * 1024  # Less than 1MB
 
     # Reload the epoch and make sure that the state was maintained
     del epoch
-    with open('epoch_test.pkl', 'rb') as f:
+    with open(filename, 'rb') as f:
         epoch = dill.load(f)
     features, targets = next(epoch)
     assert numpy.all(features == mnist.data['features'][512:768])
+
+    # Clean up
+    os.remove(filename)
