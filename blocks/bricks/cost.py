9ebcf5ec69cd90feffd910a6ff1845d23da77fbd
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-20 11:43:10 -0500

    Make Cost and CostMatrix abstract classses

--- blocks/bricks/cost.py
+++ blocks/bricks/cost.py
@@ -1,12 +1,20 @@
+from abc import ABCMeta, abstractmethod
+
 from theano import tensor
+from six import add_metaclass
 
 from blocks.bricks import application, Brick
 
 
+@add_metaclass(ABCMeta)
 class Cost(Brick):
-    pass
+    @abstractmethod
+    @application
+    def apply(self, y, y_hat):
+        pass
 
 
+@add_metaclass(ABCMeta)
 class CostMatrix(Cost):
     """Base class for costs which can be calculated element-wise.
 
@@ -17,6 +25,11 @@ class CostMatrix(Cost):
     def apply(self, y, y_hat):
         return self.cost_matrix.application_method(
             self, y, y_hat).sum(axis=1).mean()
+
+    @abstractmethod
+    @application
+    def cost_matrix(self, y, y_hat):
+        pass
 
 
 class BinaryCrossEntropy(CostMatrix):
