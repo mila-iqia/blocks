c427dd66d0a88f44f168776206d27b3b02671457
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-19 19:19:50 -0500

    Don't use HIGHEST_PROTOCOL

--- blocks/datasets/__init__.py
+++ blocks/datasets/__init__.py
@@ -155,14 +155,14 @@ class InMemoryDataset(Dataset):
     files are loaded after de-serialization, before the :meth:`load` method
     is ever called.
 
-    >>> import pickle
+    >>> import dill
     >>> from blocks.datasets.mnist import MNIST
     >>> mnist = MNIST('train')
     >>> print("{:,d} KB".format(
     ...     mnist.data['features'].nbytes / 1024)) # doctest: +SKIP
     183,750 KB
     >>> with open('mnist.pkl', 'wb') as f:
-    ...     pickle.dump(mnist, f, protocol=pickle.HIGHEST_PROTOCOL)
+    ...     dill.dump(mnist, f)
 
     You will notice that the dumping of the dataset was relatively quick,
     because it didn't attempt to write MNIST to disk. We can now reload it,
@@ -170,7 +170,7 @@ class InMemoryDataset(Dataset):
     happened.
 
     >>> with open('mnist.pkl', 'rb') as f:
-    ...     mnist = pickle.load(f)
+    ...     mnist = dill.load(f)
     >>> print(mnist.data['features'].shape)
     (60000, 784)
 
@@ -181,7 +181,7 @@ class InMemoryDataset(Dataset):
     >>> correct_path = config.data_path
     >>> config.data_path = '/non/existing/path'
     >>> with open('mnist.pkl', 'rb') as f:
-    ...     mnist = pickle.load(f)
+    ...     mnist = dill.load(f)
     >>> print(mnist.data['features'].shape) # doctest: +SKIP
     Traceback (most recent call last):
       ...
