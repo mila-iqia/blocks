b72e5008eee9a30ae60fd252cc89d892f558e864
rizar <dimabv@tut.by>
2015-01-21 12:49:00 +0100

    Separate the AggregationBuffer class
    Yet another major refactoring of the DatasetEvaluator

--- blocks/monitoring/evaluators.py
+++ blocks/monitoring/evaluators.py
@@ -10,14 +10,117 @@ from blocks.graph import ComputationGraph
 logger = logging.getLogger()
 
 
+class AggregationBuffer(object):
+    """Intermediate results of aggregating values of Theano expressions.
+
+    Incapsulates aggregators for a list of Theano expressions. Collects
+    the respective updates and provides initialization and readout
+    routines.
+
+    .. todo::
+
+        Write a better explanation what it is.
+
+    Parameters
+    ----------
+    expressions : list
+        If a list of Theano variables. The variable names are used as
+        expression names. All the variables names must be different.
+
+    Attributes
+    ----------
+    initialization_updates : list of tuples
+        Initialization updates of the aggregators.
+    accumulation_updates : list of tuples
+        Accumulation updates of the aggregators.
+    readout_expressions : dict
+        Maps an aggregated variable into a readout expression.
+    input : list of Theano variables
+        The list of inputs needed for accumulation.
+    input_names : list of str
+        The name of the inputs needed for accumulation.
+
+    """
+    def __init__(self, expressions):
+        self.expressions = expressions
+        self.expression_names = [v.name for v in self.expressions]
+        if len(self.expression_names) < len(self.expressions):
+            raise ValueError(
+                "Expression variables should have different names")
+        self.inputs = ComputationGraph(self.expressions).inputs
+        self.input_names = [v.name for v in self.inputs]
+
+        self._initialized = False
+        self._create_aggregators()
+        self._compile()
+
+    def _create_aggregators(self):
+        """Create aggregators and collect updates."""
+        self.initialization_updates = []
+        self.accumulation_updates = []
+        self.readout_expressions = OrderedDict()
+
+        for v in self.expressions:
+            logger.debug('Expression to evaluate: %s', v.name)
+            if not hasattr(v.tag, 'aggregation_scheme'):
+                if ComputationGraph([v]).inputs == []:
+                    logger.debug('Using _DataIndependent aggregation scheme'
+                                 ' for %s since it does not depend on'
+                                 ' the data', v.name)
+                    v.tag.aggregation_scheme = _DataIndependent(variable=v)
+                else:
+                    logger.debug('Using the default (average over minibatches)'
+                                 ' aggregation scheme for %s', v.name)
+                    v.tag.aggregation_scheme = Mean(v, 1.0)
+
+            aggregator = v.tag.aggregation_scheme.get_aggregator()
+            self.initialization_updates.extend(
+                aggregator.initialization_updates)
+            self.accumulation_updates.extend(aggregator.accumulation_updates)
+            self.readout_expressions[v.name] = aggregator.readout_expression
+
+    def _compile(self):
+        """Compiles Theano functions.
+
+        .. todo::
+
+            The current compilation method does not account for updates
+            attached to `ComputationGraph` elements. Compiling should
+            be out-sourced to `ComputationGraph` to deal with it.
+
+        """
+        if self.initialization_updates:
+            self._initialize_fun = theano.function(
+                [], [], updates=self.initialization_updates)
+        else:
+            self._initialize_fun = None
+
+        self._readout_fun = theano.function(
+            [], list(self.readout_expressions.values()))
+
+    def initialize_aggregators(self):
+        """Initialize the aggregators."""
+        self._initialized = True
+        if self._initialize_fun is not None:
+            self._initialize_fun()
+
+    def get_aggregated_values(self):
+        """Readout the aggregated values."""
+        if not self._initialized:
+            raise Exception("To readout you must first initialize, then"
+                            "process batches!")
+        ret_vals = self._readout_fun()
+        return dict(zip(self.expression_names, ret_vals))
+
+
 class DatasetEvaluator(object):
     """A DatasetEvaluator evaluates many Theano expressions on a dataset.
 
     The DatasetEvaluator provides a do-it-all method, :meth:`evaluate`,
     which computes values of ``expressions`` on a dataset.
 
-    Alternatively, methods :meth:`_initialize_computation`,
-    :meth:`_process_batch`, :meth:`_readout_expressions` can be used with a
+    Alternatively, methods :meth:`initialize_aggregators`,
+    :meth:`process_batch`, :meth:`get_aggregated_values` can be used with a
     custom loop over data.
 
     The values computed on subsets of the given dataset are aggregated
@@ -38,14 +141,7 @@ class DatasetEvaluator(object):
 
     """
     def __init__(self, expressions):
-        self.expressions = OrderedDict(
-            [(var.name, var) for var in expressions])
-        if len(self.expressions) < len(expressions):
-            raise ValueError(
-                "Expression variables should have different names")
-
-        self.inputs = ComputationGraph(
-            list(self.expressions.values())).inputs
+        self.buffer_ = AggregationBuffer(expressions)
         self._compile()
 
     def _compile(self):
@@ -58,63 +154,23 @@ class DatasetEvaluator(object):
             be out-sourced to `ComputationGraph` to deal with it.
 
         """
-        self._initialize_updates = []
-        self._accumulate_updates = []
-        self._readout = OrderedDict()
-
-        for k, v in self.expressions.items():
-            logger.debug('Expression to evaluate: %s', v.name)
-            if not hasattr(v.tag, 'aggregation_scheme'):
-                if ComputationGraph([v]).inputs == []:
-                    logger.debug('Using _DataIndependent aggregation scheme'
-                                 ' for %s since it does not depend on'
-                                 ' the data', k)
-                    v.tag.aggregation_scheme = _DataIndependent(variable=v)
-                else:
-                    logger.debug('Using the default (average over minibatches)'
-                                 ' aggregation scheme for %s', k)
-                    v.tag.aggregation_scheme = Mean(v, 1.0)
-
-            aggregator = v.tag.aggregation_scheme.get_aggregator()
-            self._initialize_updates.extend(aggregator.initialization_updates)
-            self._accumulate_updates.extend(aggregator.accumulation_updates)
-            self._readout[k] = aggregator.readout_expression
-
-        if self._initialize_updates:
-            self._initialize_fun = theano.function(
-                [], [], updates=self._initialize_updates)
-        else:
-            self._initialize_fun = None
-
-        self._initialized = False
-        self._input_names = [v.name for v in self.inputs]
-
-        if self._accumulate_updates:
+        if self.buffer_.accumulation_updates:
             self._accumulate_fun = theano.function(
-                self.inputs, [], updates=self._accumulate_updates)
+                self.buffer_.inputs, [],
+                updates=self.buffer_.accumulation_updates)
         else:
             self._accumulate_fun = None
 
-        self._readout_fun = theano.function([], list(self._readout.values()))
+    def initialize_aggregators(self):
+        self.buffer_.initialize_aggregators()
 
-    def _initialize_computation(self):
-        """Initialize the aggragators to process a dataset."""
-        self._initialized = True
-        if self._initialize_fun is not None:
-            self._initialize_fun()
-
-    def _process_batch(self, batch):
-        batch = dict_subset(batch, self._input_names)
+    def process_batch(self, batch):
+        batch = dict_subset(batch, self.buffer_.input_names)
         if self._accumulate_fun is not None:
             self._accumulate_fun(**batch)
 
-    def _readout_expressions(self):
-        if not self._initialized:
-            raise Exception("To readout you must first initialize, then"
-                            "process batches!")
-        self._initialized = False
-        ret_vals = self._readout_fun()
-        return dict(zip(self.expressions.keys(), ret_vals))
+    def get_aggregated_values(self):
+        return self.buffer_.get_aggregated_values()
 
     def evaluate(self, data_stream):
         """Compute the expressions over a data stream.
@@ -130,13 +186,14 @@ class DatasetEvaluator(object):
         provided dataset.
 
         """
-        self._initialize_computation()
+        self.initialize_aggregators()
 
         if self._accumulate_fun is not None:
             for batch in data_stream.get_epoch_iterator(as_dict=True):
-                self._process_batch(batch)
+                self.process_batch(batch)
         else:
-            logger.debug('Only constant monitors are used, will not'
-                         'iterate the over data!')
+            logger.debug(
+                'Only data independent expressions were given,'
+                'will not iterate the over data!')
 
-        return self._readout_expressions()
+        return self.get_aggregated_values()
