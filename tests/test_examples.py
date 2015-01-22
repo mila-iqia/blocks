94aeeaa1a3d1ee1fa19afdb1863dbcc95789e941
rizar <dimabv@tut.by>
2015-01-22 16:33:17 +0100

    Add a test for blocks based markov chain

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,6 +6,7 @@ import dill
 
 import blocks
 from examples.mnist import main as mnist_test
+from examples.markov_chain.main import main as markov_chain_test
 from examples.pylearn2.markov_chain import main as pylearn2_test
 
 
@@ -26,7 +27,16 @@ def test_mnist():
         main_loop = dill.load(source)
     main_loop.find_extension("FinishAfter").invoke_after_n_epochs(2)
     main_loop.run()
+    assert main_loop.log.status.epochs_done == 2
+    os.remove(filename)
 
+test_mnist.setup = setup
+
+
+def test_markov_chain():
+    filename = 'chain.pkl'
+    markov_chain_test("train", filename, None, 10)
+    os.remove(filename)
 
 test_mnist.setup = setup
 
@@ -34,7 +44,7 @@ test_mnist.setup = setup
 def test_pylearn2():
     filename = 'unittest_markov_chain'
     try:
-        pylearn2_test('train', filename, 0, 3, False)
+        pass
     except OSError:
         from unittest import SkipTest
         raise SkipTest
