4075e0f83af92567c6e9a4b8c6b09ef9f3029019
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-28 13:27:49 -0500

    More documentation and safer defaults

--- blocks/extensions/plot.py
+++ blocks/extensions/plot.py
@@ -15,14 +15,41 @@ logger = logging.getLogger(__name__)
 
 
 class Plot(SimpleExtension):
-    """Plot values from the log using the Bokeh plotting server.
+    """Live plotting of monitoring channels.
+
+    This extension uses Bokeh server to give you access to live updated
+    plots of the monitoring values you specify. The best thing to do is to
+    start the Bokeh server manually by running the command:
+
+    .. code-block:: bash
+
+       bokeh-server
+
+    This should make your plots accesible at ``http://localhost:5006``. If
+    you want to look at your plots over the internet or a network, use
+
+    .. code-block:: bash
+
+       bokeh-server --ip 0.0.0.0
+
+    By default the Bokeh server stores your plots, so you won't lose them
+    if you stop and restart the server.
+
+    Alternatively, you can set the ``start_server`` argument of this
+    extension to ``True``, to automatically start a server when training
+    starts. However, in that case your plots will be deleted when you shut
+    down the plotting server!
 
     .. warning::
 
-       By default this extension will try to start the Bokeh plotting
-       server. To make sure that you do not lose your plots the moment
-       training completes, this extension will not shut down the plotting
-       server. You must do so manually (the PID will be shown in the logs).
+       When starting the server automatically using the ``start_server``
+       argument, the extension won't attempt to shut down the server at the
+       end of training (to make sure that you do not lose your plots the
+       moment training completes). You have to shut it down manually (the
+       PID will be shown in the logs). If you don't do this, this extension
+       will crash when you try and train another model with
+       ``start_server`` set to ``True``, because it can't run two servers
+       at the same time.
 
     Parameters
     ----------
@@ -41,10 +68,10 @@ class Plot(SimpleExtension):
         running experiments non-locally (e.g. on a cluster or through SSH).
     start_server : bool, optional
         Whether to try and start the Bokeh plotting server. Defaults to
-        ``True``. The server started is not persistent i.e. after shutting
+        ``False``. The server started is not persistent i.e. after shutting
         it down you will lose your plots. If you want to store your plots,
-        start the server manually using the ``bokeh-server`` command and
-        set this argument to ``False``. Also see the warning above.
+        start the server manually using the ``bokeh-server`` command. Also
+        see the warning above.
 
     Notes
     -----
@@ -59,8 +86,8 @@ class Plot(SimpleExtension):
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
               '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
-    def __init__(self, document, monitors, open_browser=True,
-                 start_server=True, **kwargs):
+    def __init__(self, document, monitors, open_browser=False,
+                 start_server=False, **kwargs):
         if not bokeh_available:
             raise ImportError
         self.plots = {}
