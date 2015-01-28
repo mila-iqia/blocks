53692ff302ec45b740b29ceac7b904db961d20e5
rizar <dimabv@tut.by>
2015-01-28 16:24:15 +0100

    Handle interruptions plus docs

--- blocks/extensions/__init__.py
+++ blocks/extensions/__init__.py
@@ -304,40 +304,95 @@ class Printing(SimpleExtension):
 
 
 class Timing(TrainingExtension):
-    """Keeps track of CPU time used.
+    """Keeps track of time used.
+
+    Depending of the `clock_function` parameter this extension
+    can track both CPU or user time.
+
+    It is highly recommended to put this extension first in the extension
+    list. Assuming that this recommendation is respected, the semantics
+    of the records it writes to the log is explained below:
+
+    * `initilialization_took`: number of seconds the initialization took.
+      Includes time spent running `before_training` callbacks.
+
+    * `iteration_took`: number of seconds an iteration took. Includes
+      time spent running `after_batch` callbacks at the previous iteration
+      and `before_batch` callbacks of current iteration
+
+    * `epoch_took`: number of seconds an epoch took. Includes
+      time spent running `after_epoch` callbacks at the previous iteration
+      and `before_epoch` callbacks of current iteration
+
+    * `total_took`: number of seconds running until the current iteration
+      took.
+
+    * `final_total_took`: total number of seconds spent on training
+      including all extension calls except `after_training`.
+
+    Notes
+    -----
+    When training is interrupted this extension saves intermediate
+    time measurements to the training status, i.e. it should be robust
+    to any training interruptions.
 
     Parameters
     ----------
     clock_function : callable, optional
-        Return the current time. By default `time.clock()` ised,
-        which means that CPU time is tracked.
+        Return the current time. By default `time.time` ised,
+        which means that user time is tracked.
 
     """
     def __init__(self, clock_function=None):
         if not clock_function:
-            clock_function = time.clock
+            clock_function = time.time
         self.clock_function = clock_function
+
+    @property
+    def log(self):
+        return self.main_loop.log
 
     def before_training(self):
         self.started_at = self.clock_function()
+        self.log.status._epoch_before_interrupted = 0
+        self.log.status._total_before_interrupted = 0
 
     def before_epoch(self):
         self.epoch_started_at = self.clock_function()
-        if self.main_loop.log.status.epochs_done == 0:
-            self.main_loop.log.current_row.initialization_took = (
+        if self.log.status.epochs_done == 0:
+            self.log.current_row.initialization_took = (
                 self.epoch_started_at - self.started_at)
 
     def before_batch(self, batch):
         self.batch_started_at = self.clock_function()
 
     def after_batch(self, batch):
-        self.main_loop.log.current_row.iteration_took = (
+        self.log.current_row.iteration_took = (
             self.clock_function() - self.batch_started_at)
+        self.log.current_row.total_took = (
+            self.log.status._total_before_interrupted +
+            self.clock_function() - self.started_at)
 
     def after_epoch(self):
-        self.main_loop.log.current_row.epoch_took = (
+        self.log.current_row.epoch_took = (
+            self.log.status._epoch_before_interrupted +
             self.clock_function() - self.epoch_started_at)
+        self.log.status._epoch_before_interrupted = 0
 
     def after_training(self):
-        self.main_loop.log.current_row.training_took = (
+        self.log.current_row.final_total_took = (
+            self.log.status._total_before_interrupted +
             self.clock_function() - self.started_at)
+
+        # Save intermediate results to the log.status
+        self.log.status._total_before_interrupted = (
+            self.log.current_row.final_total_took)
+        if self.log.status._epoch_started:
+            epoch_ends = self.log.status._epoch_ends
+            self.log.status._epoch_before_interrupted = (
+               self.clock_function() -
+               0 if not epoch_ends else self.log[epoch_ends[-1]].total_took)
+
+    def on_resumption(self):
+        self.started_at = self.clock_function()
+        self.epoch_started_at = self.clock_function()
