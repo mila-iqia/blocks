ed0ecdedb4b18c9bfa2391f8302a86fe36e06bee
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-29 09:40:39 -0500

    imports yaml, but pyyaml on pipy...

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     keywords='theano machine learning neural networks deep learning',
     packages=find_packages(exclude=['examples', 'docs', 'tests']),
     scripts=['bin/blocks-continue'],
-    install_requires=['dill', 'numpy', 'theano', 'six', 'yaml'],
+    install_requires=['dill', 'numpy', 'theano', 'six', 'pyyaml'],
     extras_require={
         'test': ['nose', 'nose2'],
         'plot': ['bokeh']
