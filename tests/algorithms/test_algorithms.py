f099b2497ca12ebaa5ba262414d35a6e1cfdd36f
rizar <dimabv@tut.by>
2015-02-02 11:45:22 +0100

    Gradient clipping, shared learning rate

--- tests/algorithms/test_algorithms.py
+++ tests/algorithms/test_algorithms.py
@@ -3,7 +3,7 @@ from numpy.testing import assert_allclose
 
 from theano import tensor
 
-from blocks.algorithms import GradientDescent
+from blocks.algorithms import GradientDescent, GradientClipping
 from blocks.utils import shared_floatx
 
 
@@ -17,3 +17,16 @@ def test_gradient_descent():
     algorithm.initialize()
     algorithm.process_batch(dict())
     assert_allclose(W.get_value(), -0.5 * W_start_value)
+
+
+def test_gradient_clipping():
+    rule1 = GradientClipping(4)
+    rule2 = GradientClipping(5)
+
+    gradients = {0: shared_floatx(3.0), 1: shared_floatx(4.0)}
+    clipped1 = rule1.compute_steps(gradients)
+    assert_allclose(clipped1[0].eval(), 12 / 5.0)
+    assert_allclose(clipped1[1].eval(), 16 / 5.0)
+    clipped2 = rule2.compute_steps(gradients)
+    assert_allclose(clipped2[0].eval(), 3.0)
+    assert_allclose(clipped2[1].eval(), 4.0)
