d65e479f6d5281511826179c04bfaf25c8c63a77
dima <serdyuk.dmitriy@gmail.com>
2015-02-11 17:36:18 -0500

    Improved documentation

--- blocks/extensions/training.py
+++ blocks/extensions/training.py
@@ -2,7 +2,10 @@ from blocks.extensions import TrainingExtension
 
 
 class AdjustParameter(TrainingExtension):
-    """Adjusts shared variable parameter using some function each iteration
+    """Adjusts shared variable parameter using some function.
+
+    Applies function to compute the new value of a shared parameter each
+    iteration.
 
     This class can be used to adapt over the training process parameters like
     learning rate, momentum, etc.
@@ -12,6 +15,7 @@ class AdjustParameter(TrainingExtension):
     parameter : shared variable to be adjusted
     function : function which input number of aggregated examples and outputs
                the parameter value
+
     """
     def __init__(self, parameter, function, **kwargs):
         super(AdjustParameter, self).__init__(**kwargs)
@@ -22,4 +26,3 @@ class AdjustParameter(TrainingExtension):
     def after_batch(self, batch):
         self.num_examples += batch.values()[0].shape[0]
         self.learning_rate.set_value(self.function(self.num_examples))
-
