e1cc793dd6a5822583596be6cd23d9d0516b4bb5
Vincent Dumoulin <vi.dumoulin@gmail.com>
2015-02-11 10:42:38 -0500

    Fix typos in RMSPropBase

--- blocks/algorithms/__init__.py
+++ blocks/algorithms/__init__.py
@@ -419,9 +419,10 @@ class RMSPropBase(StepRule):
 
     def compute_step(self, param, gradient):
         mean_square_grad_tm1 = shared_floatx(param.get_value() * 0.)
-        mean_squared_grad_t = (self.decay_rate * mean_square_grad_tm1 +
-                               (1 - self.decay_rate) * T.sqr(param))
-        rms_grad_t = T.maximum(T.sqrt(mean_squared_grad_t), self.epsilon)
+        mean_square_grad_t = (self.decay_rate * mean_square_grad_tm1 +
+                              (1 - self.decay_rate) * tensor.sqr(param))
+        rms_grad_t = tensor.maximum(
+            tensor.sqrt(mean_square_grad_t), self.epsilon)
         step = gradient / rms_grad_t
         updates = [(mean_square_grad_t, rms_grad_t)]
         return step, updates
