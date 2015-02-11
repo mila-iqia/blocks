3c1bf97260d3c0d7977c94890e2cc7ae016e12c4
Vincent Dumoulin <vi.dumoulin@gmail.com>
2015-02-10 23:15:24 -0500

    Put import in alphabetical order

--- tests/algorithms/test_algorithms.py
+++ tests/algorithms/test_algorithms.py
@@ -3,8 +3,8 @@ from collections import OrderedDict
 import numpy
 import theano
 from numpy.testing import assert_allclose
-from theano import tensor
 from nose.tools import assert_raises
+from theano import tensor
 
 from blocks.algorithms import (GradientDescent, GradientClipping,
                                CompositeRule, SteepestDescent,
