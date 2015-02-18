fe1f323e4be4dcbdaeae3747027ac5333830bb37
Jorg Bornschein <jb@capsec.org>
2015-02-18 11:08:47 -0500

    Add tests for scripts/plot.py

---
+++ tests/scripts/test_plot.py
@@ -0,0 +1,43 @@
+
+
+import blocks.scripts.plot as plot
+
+from collections import OrderedDict
+from unittest import skipIf
+#from blocks.log import TrainingLog
+#from tests import silence_printing
+
+try:
+    from pandas import DataFrame
+    PANDAS_AVAILABLE = True
+except:
+    PANDAS_AVAILABLE = False
+
+
+def some_experiments():
+    """Create some 2 dummy experiments."""
+    experiments = OrderedDict()
+    experiments['exp0'] = DataFrame()
+    experiments['exp0']['col0'] = (0,1,2)
+    experiments['exp0']['col1'] = (3,4,5)
+    experiments['exp1'] = DataFrame()
+    experiments['exp1']['col0'] = (6,7,8)
+    experiments['exp1']['col1'] = (9,9,9)
+    return experiments
+
+
+#@silence_printing
+@skipIf(not PANDAS_AVAILABLE, "pandas not installed")
+def test_print_column_summary():
+    experiments = some_experiments()
+    plot.print_column_summary(experiments)
+
+
+@skipIf(not PANDAS_AVAILABLE, "pandas not installed")
+def test_match_column_specs():
+    experiments = some_experiments()
+    specs = ['0:col0', '*1']
+    df = plot.match_column_specs(experiments, specs)
+
+    assert isinstance(df, DataFrame)
+    assert list(df.columns) == ['0:col0', '0:col1', '1:col1']
