b27950e33448308c681d8b5a7a62ea5f58acb2b8
Jorg Bornschein <jb@capsec.org>
2015-02-18 11:21:25 -0500

    Update tests

--- tests/scripts/test_plot.py
+++ tests/scripts/test_plot.py
@@ -3,9 +3,7 @@
 import blocks.scripts.plot as plot
 
 from collections import OrderedDict
-from unittest import skipIf
-#from blocks.log import TrainingLog
-#from tests import silence_printing
+from tests import silence_printing, skip_if_not_available
 
 try:
     from pandas import DataFrame
@@ -18,23 +16,23 @@ def some_experiments():
     """Create some 2 dummy experiments."""
     experiments = OrderedDict()
     experiments['exp0'] = DataFrame()
-    experiments['exp0']['col0'] = (0,1,2)
-    experiments['exp0']['col1'] = (3,4,5)
+    experiments['exp0']['col0'] = (0, 1, 2)
+    experiments['exp0']['col1'] = (3, 4, 5)
     experiments['exp1'] = DataFrame()
-    experiments['exp1']['col0'] = (6,7,8)
-    experiments['exp1']['col1'] = (9,9,9)
+    experiments['exp1']['col0'] = (6, 7, 8)
+    experiments['exp1']['col1'] = (9, 9, 9)
     return experiments
 
 
-#@silence_printing
-@skipIf(not PANDAS_AVAILABLE, "pandas not installed")
+@silence_printing
 def test_print_column_summary():
+    skip_if_not_available(modules=['pandas'])
     experiments = some_experiments()
     plot.print_column_summary(experiments)
 
 
-@skipIf(not PANDAS_AVAILABLE, "pandas not installed")
 def test_match_column_specs():
+    skip_if_not_available(modules=['pandas'])
     experiments = some_experiments()
     specs = ['0:col0', '*1']
     df = plot.match_column_specs(experiments, specs)
