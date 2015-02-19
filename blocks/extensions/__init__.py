1524184725d74f026f4b96288a3da5441358345c
Jorg Bornschein <jb@capsec.org>
2015-02-19 10:46:39 -0500

    Add ProgressBar extension

--- blocks/extensions/__init__.py
+++ blocks/extensions/__init__.py
@@ -343,6 +343,59 @@ class Printing(SimpleExtension):
         print()
 
 
+class ProgressBar(TrainingExtension):
+    """Display a progress bar during training.
+
+    This extension tries to obtain the number of mini-batches processed per
+    epoch and will display progress bar. Not all :class:`IterationScheme`
+    classes provide the necessary atributes.
+
+    Notes
+    -----
+    This extension should be run before other extensions that print to the screen
+    at the end or at the beginning of the epoch (e.g. the Printing extension).
+    Placing ProgressBar before these extension will ensure you won't get
+    intermingled output on your terminal.
+
+    """
+    def __init__(self, **kwargs):
+        super(ProgressBar, self).__init__(**kwargs)
+
+        self.bar = None
+        self.epoch_counter = 0
+        self.batch_counter = 0
+
+    def before_epoch(self):
+        self.epoch_counter += 1
+        self.batch_counter = 0
+
+        iteration_scheme = self.main_loop.data_stream.iteration_scheme
+        if hasattr(iteration_scheme, 'batches_per_epoch'):
+            batches_per_epoch = iteration_scheme.batches_per_epoch
+        elif hasattr(iteration_scheme, 'num_examples') and
+                hasattr(iteration_scheme, 'batch_size'):
+            batches_per_epoch = iteration_scheme.num_examples // iteration_scheme.batch_size
+        else:
+            logger.warning("Disabling ProgressBar: The training iteration scheme does not provide the necessary attributes to calculate batches_per_epoch")
+            return
+
+        widgets = [ "Epoch {}, step ".format(self.epoch_counter),
+                    progressbar.Counter(), ' (', progressbar.Percentage(), ') ',
+                    progressbar.Bar(), ' ', progressbar.Timer(), ' ', progressbar.ETA()]
+        self.bar = progressbar.ProgressBar(widgets=widgets, maxval=batches_per_epoch)
+
+    def after_epoch(self):
+        if self.bar:
+            self.bar.finish()
+
+    def before_batch(self, batch):
+        if self.bar:
+            if self.batch_counter == 0:
+                self.bar.start()
+            self.batch_counter += 1
+            self.bar.update(self.batch_counter)
+
+
 class Timing(TrainingExtension):
     """Keeps track of time used.
 
