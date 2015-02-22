c098290dd2538120183cf55dbf204c04fdcf637a
Ying Zhang <ying.zhlisa@gmail.com>
2015-02-22 11:47:53 -0500

    added docstring

--- blocks/bricks/cost.py
+++ blocks/bricks/cost.py
@@ -25,7 +25,23 @@ class Softmax(Brick):
 
     @application
     def categorical_cross_entropy(self, x, y):
-        x =  x - x.max(axis=1).dimshuffle(0, 'x')
+        """
+        Return computationally stable softmax cost.
+
+
+        :type x: matrix
+        :param x: Each slice along axis represents one distribution.
+
+
+        :type y: floating point matrix or integer vector
+        :param y: In the case of a matrix argument, each slice along axis represents
+        one distribution. In the other case, each element represents the position of
+        the '1' in a one hot-vector.
+
+
+        """
+
+        x = x - x.max(axis=1).dimshuffle(0, 'x')
         log_prob = x - tensor.log(tensor.exp(x).sum(axis=1).dimshuffle(0, 'x'))
         cost = tensor.nnet.categorical_crossentropy(log_prob, y).mean()
         return cost
