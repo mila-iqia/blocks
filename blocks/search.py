b018a3acaabebda745b660e663879f7fbf5bede5
dima <serdyuk.dmitriy@gmail.com>
2015-02-21 22:18:20 -0500

    Function compilation with probabilities

--- blocks/search.py
+++ blocks/search.py
@@ -1,6 +1,6 @@
-from blocks.utils import dict_union
 
 from abc import ABCMeta, abstractmethod
+from collections import OrderedDict
 
 import numpy as np
 
@@ -9,6 +9,11 @@ from theano import config
 from theano import tensor
 
 from blocks.bricks.sequence_generators import BaseSequenceGenerator
+from blocks.filter import VariableFilter
+from blocks.graph import ComputationGraph
+from blocks.utils import dict_union
+
+floatX = config.floatX
 
 
 class Search(object):
@@ -58,20 +63,23 @@ class BeamSearch(Search):
     sequence_generator : a sequence generator brick
 
     """
-    def __init__(self, beam_size, batch_size, sequence_generator, x, y, x_mask,
-                 y_mask, f_input, inputs):
+    def __init__(self, beam_size, batch_size, sequence_generator, attended,
+                 y, attended_mask, y_mask, inputs_dict, inputs):
         super(BeamSearch, self).__init__(sequence_generator)
         self.beam_size = beam_size
         self.batch_size = batch_size
         self.sequence_generator = sequence_generator
-        self.x = x
+        self.attended = attended
         self.y = y
-        self.x_mask = x_mask
+        self.attended_mask = attended_mask
         self.y_mask = y_mask
-        self.f_input = f_input
+        self.inputs_dict = inputs_dict
         self.inputs = inputs
+        self.generate_names = sequence_generator.generate_outputs()
+        self.init_computer = None
+        self.next_computer = None
 
-    def compile(self, state_names, glipmse_names, *args, **kwargs):
+    def compile(self, *args, **kwargs):
         """Compiles functions for beamsearch
 
         Parameters
@@ -80,45 +88,57 @@ class BeamSearch(Search):
             Dictionary of named inputs
 
         """
-        states = {name: kwargs[name] for name
-                  in self.generator.state_names}
-        contexts = {name: kwargs[name] for name
-                    in self.generator.context_names}
-        glimpses = {name: kwargs[name] for name
-                    in self.generator.glimpse_names}
-        input_dict = dict_union(states, contexts, glimpses)
-        self.state_names = states.keys()
-        self.context_names = contexts.keys()
-        self.glimpse_names = glimpses.keys()
+        generator = self.sequence_generator
+        attended = self.attended
+        attended_mask = self.attended_mask
+        init_generated = generator.generate(attended=attended,
+                                            attended_mask=attended_mask,
+                                            iterate=False, n_steps=1,
+                                            batch_size=self.batch_size)
+        init_generated = OrderedDict(zip(self.generate_names, init_generated))
+        init_cg = ComputationGraph(init_generated.values())
+        init_readouts = VariableFilter(application=generator.readout.emit,
+                                       name='readouts')(init_cg.variables)[-1]
+        init_probs = generator.readout.emitter._probs(init_readouts)
 
-        init_state, init_out, init_costs, init_probs = \
-            self.generator.generate(iterate=False, batch_size=self.batch_size,
-                                    n_steps=1, contexts=contexts,
-                                    output_probs=True,
-                                    return_initial_states=True,
-                                    return_dict=True)
-        self.init_state_computer = function(self.inputs.values(),
-                                            [init_state, init_out, init_costs,
-                                             init_probs])
+        self.init_computer = function(self.inputs,
+                                      init_generated.values() + [init_probs])
 
-        self.outputs = tensor.TensorType('int64', (False,) *
-                                     self.generator.get_dim("outputs"))()
+        cur_variables = OrderedDict()
+        for name, value in init_generated.iteritems():
+            cur_value = tensor.zeros_like(value)
+            cur_value.name = name
+            cur_variables[name] = cur_value
 
-        curr_out = tensor.zeros_like(init_out)
-        curr_states = tensor.zeros_like(init_state)
-        next_state, next_out, (next_costs, next_probs) = \
-            self.generator.generate(outputs=curr_out[None, :],
-                                    states=curr_states,
-                                    iterate=False,
-                                    batch_size=self.batch_size, n_steps=1,
-                                    contexts=contexts, output_probs=True,
-                                    return_dict=True)
-
-        self.next_state_computer = function([curr_out, curr_states],
-                                            [next_state, next_out, next_costs,
-                                             next_probs])
-
+        input_names = (generator.state_names + generator.glimpse_names +
+                       ['outputs'])
+        generator_inputs = {name: val for name, val
+                            in cur_variables.iteritems()
+                            if name in input_names}
+        next_generated = generator.generate(attended=attended,
+                                            attended_mask=attended_mask,
+                                            iterate=False,
+                                            n_steps=1,
+                                            batch_size=self.batch_size,
+                                            **generator_inputs)
+        next_generated = OrderedDict(zip(self.generate_names, next_generated))
+        cg_step = ComputationGraph(next_generated.values())
+        readouts_step = VariableFilter(application=generator.readout.emit,
+                                       name='readouts')(cg_step.variables)[-1]
+        next_probs = generator.readout.emitter._probs(readouts_step)
+        self.next_computer = function(self.inputs.values() +
+                                      [cur_variables['states']],
+                                      next_generated.values() + [next_probs])
         super(BeamSearch, self).compile(*args, **kwargs)
+
+    def compute_initial(self, attended, attended_mask):
+        inits = self.init_computer(attended, attended_mask)
+        return OrderedDict(zip(self.generate_names + ['probs'], inits))
+
+    def compute_next(self, attended, attended_mask, cur_vals):
+        next_val = self.next_computer([attended, attended_mask,
+                                       cur_vals['states']])
+        return OrderedDict(zip(self.generate_names + ['probs'], next_val))
 
     @classmethod
     def _top_probs(cls, probs, beam_size, unique=False):
@@ -144,6 +164,15 @@ class BeamSearch(Search):
         indexes = np.unravel_index(args, probs.shape)
         return indexes, probs[indexes]
 
+    @classmethod
+    def _rearrange(cls, outputs, indexes):
+        n_chunks = indexes.shape[0]
+        beam_size = indexes.shape[1]
+        new_outputs = outputs.reshape((-1, n_chunks * beam_size))
+        new_outputs = new_outputs[:, indexes.flatten()]
+        new_outputs = new_outputs.reshape(outputs.shape)
+        return new_outputs.copy()
+
     def search(self, start_symbol, eol_symbol=-1, max_length=512,
                **kwargs):
         """Performs greedy search
@@ -163,51 +192,66 @@ class BeamSearch(Search):
 
         """
         super(BeamSearch, self).search(**kwargs)
-        context_vals = {name: val for name, val in kwargs.iteritems() if name
-                        in self.context_names}
+        attended_vals = kwargs['attended']
+        attended_mask_vals = kwargs['attended_mask']
 
-        n_chunks = list(context_vals.values())[0].shape[1] # input batch size
+        n_chunks = attended_vals.shape[1] # input batch size
         real_batch_size = n_chunks * self.beam_size
 
-        aux_inputs = {name: np.tile(val, self.beam_size) for name, val
-                      in context_vals.iteritems()}
-        aux_inp = np.tile(inp_seq, (1, self.beam_size, 1))
-        aux_mask = np.tile(inp_mask, (1, self.beam_size))
+        aux_attended = np.tile(attended_vals, self.beam_size)
+        aux_attended_mask = np.tile(attended_mask_vals, self.beam_size)
 
         current_outputs = np.zeros((0, n_chunks, self.beam_size),
                                    dtype='int64')
         curr_out_mask = np.ones((0, n_chunks, self.beam_size), dtype=floatX)
 
-        outputs_dim = self.generator.get_dim('outputs')
-        current_outputs = np.array(outputs_dim)  # TODO: outputs dimensionality
         for i in xrange(max_length):
-            # Compute probabilities
-            next_glimpses = self.next_glimpse_computer(inputs.values())
-            next_readouts = self.next_readouts_computer(inputs.values())
-
-            next_outputs = self.next_outputs_computer(inputs.values() +
-                                                      next_readouts.values() +
-                                                      [next_glimpses])
-            next_states = [computer(inputs.values() +
-                                    next_readouts.values() +
-                                    [next_glimpses])
-                           for computer in self.next_states_computers]
-            next_costs = self.next_costs_computer(inputs.values() +
-                                                  next_readouts.values() +
-                                                  [next_glimpses])
-            next_probs = self.next_probs_computer(next_readouts)
+            if i == 0:
+                cur_values = self.compute_initial(aux_attended,
+                                                  aux_attended_mask)
+                next_probs = cur_values['probs']
+            else:
+                cur_values = self.compute_next(aux_attended, aux_attended_mask,
+                                               cur_values)
+                next_probs = cur_values['probs']
 
             # Choose top beam_size
-            prob_batches = self._chunks(next_probs, self.batch_size)
+            prob_batches = next_probs.reshape((n_chunks, self.beam_size, -1))
             # Top probs
-            indexes, top_probs = zip(*[self._top_probs(batch, self.beam_size)
+            indexes, top_probs = zip(*[self._top_probs(batch, self.beam_size,
+                                                       unique=i == 0)
                                        for batch in prob_batches])
-            outputs = [ind[-1] for ind in indexes]
-            #current_outputs.
-            # Next state
-            # Next output
+            indexes = np.array(indexes)  # chunk, 2, beam
+            # current_outputs.
+            # here we suppose, that we have 2d outputs
+            outputs = indexes[:, 1, :].copy()
 
-            # if all meet eol
-            if False:
+            # rearrange outputs
+            rearrange_ind = indexes[:, 0, :]
+            current_outputs = self._rearrange(current_outputs, rearrange_ind)
+            curr_out_mask = self._rearrange(curr_out_mask, rearrange_ind)
+            for name in cur_values:
+                cur_values[name] = self._rearrange(cur_values[name],
+                                                   rearrange_ind)
+
+            # construct next output
+            outputs = outputs.reshape((1, n_chunks, self.beam_size))
+            current_outputs = np.append(current_outputs,
+                                        outputs.copy(), axis=0)
+            # check if we meet eol
+            next_out_mask = np.ones((1, n_chunks, self.beam_size),
+                                    dtype=floatX)
+
+            next_out_mask[0, :, :] = (outputs[0, :, :] != eol_symbol)
+            curr_out_mask = np.append(curr_out_mask, next_out_mask.copy(),
+                                      axis=0)
+
+            if np.all(current_outputs[-1, :, 0] == eol_symbol):
                 break
+
+        # Select only best
+        current_outputs = current_outputs[:, :, 0]
+        curr_out_mask = curr_out_mask[:, :, 0]
+
+        return current_outputs, curr_out_mask
         raise NotImplementedError()  # TODO: remove when implemented
