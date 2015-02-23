3a85aabc68ce7de5c06a2150f8bdd322ec168876
dima <serdyuk.dmitriy@gmail.com>
2015-02-23 11:13:16 -0500

    Added time dimension in test_search

--- tests/test_search.py
+++ tests/test_search.py
@@ -7,7 +7,7 @@ from blocks.search import BeamSearch
 
 def test_top_probs():
     """Test top probabilities"""
-    a = np.array([[3, 6, 4], [1, 2, 7]])
+    a = np.array([[[3, 6, 4], [1, 2, 7]]])
     ind, maxs = BeamSearch._top_probs(a, 2)
     assert np.all(np.array(ind) == (np.array([[1, 0], [2, 1]])))
     assert np.all(maxs == [7, 6])
