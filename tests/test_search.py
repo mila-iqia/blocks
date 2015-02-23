2987cbc6983eb15aab8dd9e26ace16200d653047
dima <serdyuk.dmitriy@gmail.com>
2015-02-23 11:14:18 -0500

    Refactored search

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,14 +1,12 @@
-__author__ = 'serdyuk'
-
-import numpy as np
+import numpy
 
 from blocks.search import BeamSearch
 
 
 def test_top_probs():
     """Test top probabilities"""
-    a = np.array([[[3, 6, 4], [1, 2, 7]]])
+    a = numpy.array([[[3, 6, 4], [1, 2, 7]]])
     ind, maxs = BeamSearch._top_probs(a, 2)
-    assert np.all(np.array(ind) == (np.array([[1, 0], [2, 1]])))
-    assert np.all(maxs == [7, 6])
+    assert numpy.all(numpy.array(ind) == (numpy.array([[1, 0], [2, 1]])))
+    assert numpy.all(maxs == [7, 6])
 
