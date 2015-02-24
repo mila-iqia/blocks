fcd55f450dd563abe9be52f33549331beb918e08
Ying Zhang <ying.zhlisa@gmail.com>
2015-02-24 00:55:27 -0500

    fixed flake8 errors

--- tests/bricks/test_cost.py
+++ tests/bricks/test_cost.py
@@ -8,6 +8,7 @@ from theano import function
 from blocks.bricks import Softmax
 from blocks.bricks.cost import CategoricalCrossEntropy
 
+
 def test_softmax_vector():
     """
     return computationally stable softmax cost whose target is a label vector.
@@ -27,8 +28,10 @@ def test_softmax_vector():
     batch_size = 100
     x_size = 10
 
-    x_val = numpy.asarray(numpy.random.randn(batch_size, x_size), dtype = theano.config.floatX)
-    y_val = numpy.random.randint(low=0, high=x_size, size=(batch_size)).astype(int)
+    x_val = numpy.asarray(numpy.random.randn(batch_size, x_size),
+                          dtype=theano.config.floatX)
+    y_val = numpy.random.randint(low=0, high=x_size,
+                                 size=(batch_size)).astype(int)
     softmax_cost = softmax_cost_func(x_val, y_val)
     softmax_cost_stable = softmax_cost_stable_func(x_val, y_val)
 
@@ -37,7 +40,8 @@ def test_softmax_vector():
 
 def test_softmax_matrix():
     """
-    return computationally stable softmax cost whose target is a distribution matrix.
+    return computationally stable softmax cost whose target
+    is a distribution matrix.
     """
 
     x = tensor.matrix('x')
@@ -54,9 +58,11 @@ def test_softmax_matrix():
     batch_size = 2
     x_size = 2
 
-    x_val = numpy.asarray(numpy.random.randn(batch_size, x_size), dtype = theano.config.floatX)
-    y_val_unscaled = numpy.asarray(numpy.random.uniform(size=(batch_size, x_size)))
-    y_val = y_val_unscaled / numpy.expand_dims(y_val_unscaled.sum(axis=1), axis=1)
+    x_val = numpy.asarray(numpy.random.randn(batch_size, x_size),
+                          dtype=theano.config.floatX)
+    y_val_unscaled = numpy.asarray(numpy.random.uniform((batch_size, x_size)))
+    y_val = y_val_unscaled / numpy.expand_dims(y_val_unscaled.sum(axis=1),
+                                               axis=1)
     softmax_cost = softmax_cost_func(x_val, y_val)
     softmax_cost_stable = softmax_cost_stable_func(x_val, y_val)
 
