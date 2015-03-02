ac7c062ae171aad5ff0672df950b3d16ad246bf8
Eder Santana <edersantanajunior@hotmail.com>
2015-03-02 16:31:15 -0500

    Update server_url description
    correct formatting

--- blocks/extensions/plot.py
+++ blocks/extensions/plot.py
@@ -59,9 +59,9 @@ class Plot(SimpleExtension):
         start the server manually using the ``bokeh-server`` command. Also
         see the warning above.
     server_url : str, optional
-        Url of the bokeh-server. Ex: when starting the
-        bokeh-server with ``bokeh-server --ip 0.0.0.0`` at ``alice``, server_url
-        should be ``http://alice:5006``.
+        Url of the bokeh-server. Ex: when starting the bokeh-server with 
+        ``bokeh-server --ip 0.0.0.0`` at ``alice``, server_url should be 
+        ``http://alice:5006``.
 
     """
     # Tableau 10 colors
