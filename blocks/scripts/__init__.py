768549b19352647b44a9af40cd41432850715f88
rizar <dimabv@tut.by>
2015-03-03 14:29:48 +0100

    Make 'dump_path' optional for blocks-dump

--- blocks/scripts/__init__.py
+++ blocks/scripts/__init__.py
@@ -1,3 +1,5 @@
+import os.path
+
 from six.moves import cPickle
 
 from blocks import config
@@ -12,6 +14,11 @@ def continue_training(path):
 
 
 def dump(pickle_path, dump_path):
+    if not dump_path:
+        root, ext = os.path.splitext(pickle_path)
+        if not ext:
+            raise ValueError
+        dump_path = root
     with change_recursion_limit(config.recursion_limit):
         main_loop = cPickle.load(open(pickle_path, "rb"))
     MainLoopDumpManager(dump_path).dump(main_loop)
