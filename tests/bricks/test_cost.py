Use fixed seed for softmax test

tests/bricks/test_cost.py
@@ -24,10 +24,9 @@ def test_softmax_vector():
     batch_size = 100
     x_size = 10
 
-    x_val = numpy.asarray(numpy.random.randn(batch_size, x_size),
-                          dtype=theano.config.floatX)
-    y_val = numpy.random.randint(low=0, high=x_size,
-                                 size=(batch_size)).astype(int)
+    rng = numpy.random.RandomState(1)
+    x_val = rng.randn(batch_size, x_size).astype(theano.config.floatX)
+    y_val = rng.randint(low=0, high=x_size, size=(batch_size))
     softmax_cost = softmax_cost_func(x_val, y_val)
     softmax_cost_stable = softmax_cost_stable_func(x_val, y_val)
 
@@ -49,12 +48,11 @@ def test_softmax_matrix():
     batch_size = 2
     x_size = 2
 
-    x_val = numpy.asarray(numpy.random.randn(batch_size, x_size),
-                          dtype=theano.config.floatX)
-    y_val_us = numpy.array(numpy.random.uniform(size=(batch_size, x_size)),
-                           dtype=theano.config.floatX)
-    y_val = y_val_us / numpy.expand_dims(y_val_us.sum(axis=1),
-                                         axis=1)
+    rng = numpy.random.RandomState(1)
+    x_val = rng.randn(batch_size, x_size).astype(theano.config.floatX)
+    y_val_us = rng.uniform(size=(batch_size,
+                                 x_size)).astype(theano.config.floatX)
+    y_val = y_val_us / numpy.expand_dims(y_val_us.sum(axis=1), axis=1)
     softmax_cost = softmax_cost_func(x_val, y_val)
     softmax_cost_stable = softmax_cost_stable_func(x_val, y_val)
 