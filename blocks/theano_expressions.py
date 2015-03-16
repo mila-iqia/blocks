b4dc5e8d2fc6fb82a55375df2149eaa5bf9189b9
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-03-16 16:26:19 -0400

    Add expression for Hessian times vector

--- blocks/theano_expressions.py
+++ blocks/theano_expressions.py
@@ -19,3 +19,25 @@ def l2_norm(tensors):
                  for t in flattened]
     joined = tensor.join(0, *flattened)
     return tensor.sqrt(tensor.sqr(joined).sum())
+
+
+def hessian_times_vector(gradient, parameter, vector, r_op=False):
+    """Return an expression for the Hessian times a vector.
+
+    Parameters
+    ----------
+    gradient : :class:`~tensor.TensorVariable`
+        The gradient of a cost with respect to `parameter`
+    parameter : :class:`~tensor.TensorVariable`
+        The parameter with respect to which to take the gradient
+    vector : :class:`~tensor.TensorVariable`
+        The vector with which to multiply the Hessian
+    rop : bool, optional
+        Whether to use :func:`~tensor.gradient.Rop` or not. Defaults to
+        ``False``. Which solution is fastest normally needs to be
+        determined by profiling.
+
+    """
+    if r_op:
+        return tensor.Rop(gradient, parameter, vector)
+    return tensor.grad(tensor.sum(gradient * vector), parameter)
