71e3f7523b2e9b4205cbe269d24448d4c3c92cc7
Melanie Ducoffe <ducoffem@boltzmann.iro.umontreal.ca>
2015-03-18 09:53:35 -0400

    add tests in test_variablefilter

--- tests/test_variable_filter.py
+++ tests/test_variable_filter.py
@@ -61,11 +61,7 @@ def test_variable_filter():
     assert variables == appli_filter(cg.variables)
 
     # Testing filtering by applications
-    appli_filter = VariableFilter(applications=[brick1.apply])
+    appli_filter = VariableFilter(applications=[brick1.apply, brick2.apply])
     variables = [cg.variables[1], cg.variables[8]]
     assert variables == appli_filter(cg.variables)
-
-    # Testing filtering by applications
-    appli_filter = VariableFilter(application=[brick1.apply])
-    variables = [cg.variables[1], cg.variables[8]]
-    assert variables == appli_filter(cg.variables)
+    assert_raises(ValueError, appli_filter, cg.variables)
