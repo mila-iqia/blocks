6ce51852a1e826f8d1b69b0c48402ab8d3f7a4f2
Dmitry Bogdanov <dimabgv@gmail.com>
2015-03-19 11:11:38 +0100

    Configurable initial outputs for SoftmaxEmitter

--- blocks/bricks/sequence_generators.py
+++ blocks/bricks/sequence_generators.py
@@ -477,7 +477,15 @@ class SoftmaxEmitter(AbstractEmitter, Initializable, Random):
 
     Interprets readout elements as energies corresponding to their indices.
 
+    Parameters
+    ----------
+    initial_output : int or a scalar :class:`~theano.Variable`
+        The initial output.
+
     """
+    def __init__(self, initial_output):
+        self.initial_output = output
+    
     @application
     def probs(self, readouts):
         shape = readouts.shape
@@ -507,7 +515,7 @@ class SoftmaxEmitter(AbstractEmitter, Initializable, Random):
 
     @application
     def initial_outputs(self, batch_size, *args, **kwargs):
-        return tensor.zeros((batch_size,), dtype='int64')
+        return self.initial_output * tensor.ones((batch_size,), dtype='int64')
 
     def get_dim(self, name):
         if name == 'outputs':
