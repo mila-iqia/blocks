8e08a12e891126b0d541a96127b87cf60fa2b844
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-03-25 00:17:14 -0400

    Fix bug in plotting

--- blocks/extensions/plot.py
+++ blocks/extensions/plot.py
@@ -104,7 +104,7 @@ class Plot(SimpleExtension):
         log = self.main_loop.log
         iteration = log.status['iterations_done']
         i = 0
-        for key, value in log.current_row:
+        for key, value in log.current_row.items():
             if key in self.p_indices:
                 if key not in self.plots:
                     fig = self.p[self.p_indices[key]]
