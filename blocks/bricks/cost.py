f563f9a19e7cfd5fca89591ac9c3bb135fc75a4f
rizar <dimabv@tut.by>
2015-05-15 11:54:30 +0000

    Fix 633

--- blocks/bricks/cost.py
+++ blocks/bricks/cost.py
@@ -65,5 +65,8 @@ class CategoricalCrossEntropy(Cost):
 class MisclassificationRate(Cost):
     @application(outputs=["error_rate"])
     def apply(self, y, y_hat):
-        return (tensor.sum(tensor.neq(y, y_hat.argmax(axis=1))) /
-                y.shape[0].astype(floatX))
+        # Here we have to cast both operands to floatX explicitly.
+        # Because int64 / float32 = float64 in Theano, unfortunately.
+        return (
+            tensor.sum(tensor.neq(y, y_hat.argmax(axis=1))).astype(floatX) /
+            y.shape[0].astype(floatX))
