480e6d085593d6b29929a05257915f23ecc96e6e
David Warde-Farley <wardefar@iro.umontreal.ca>
2015-05-30 19:09:55 -0400

    Add epoch support, refactored tests.

--- tests/extensions/test_stopping.py
+++ tests/extensions/test_stopping.py
@@ -1,67 +1,98 @@
+import unittest
 from blocks.extensions.stopping import FinishIfNoImprovementAfter
 
 
 class FakeLog(object):
-    current_row = {}
-    status = {'iterations_done': 0}
+    epoch_length = 4
+
+    def __init__(self):
+        self.current_row = {}
+        self.status = {'iterations_done': 0, 'epochs_done': 0}
+
+    def advance(self, epochs):
+        self.status['iterations_done'] += self.epoch_length if epochs else 1
+        self.status['epochs_done'] += (1 if epochs
+                                       else (self.status['iterations_done'] //
+                                             self.epoch_length))
 
 
 class FakeMainLoop(object):
-    log = FakeLog()
+    def __init__(self):
+        self.log = FakeLog()
 
 
-def test_finish_if_no_improvement_after():
-    main_loop = FakeMainLoop()
+class FinishIfNoImprovementAfterTester(unittest.TestCase):
+    def check_not_stopping(self):
+        finish = self.main_loop.log.current_row.get(
+            'training_finish_requested', False)
+        self.assertFalse(finish)
 
-    def check_not_stopping():
-        finish = main_loop.log.current_row.get('training_finish_requested',
-                                               False)
-        assert not finish
+    def check_stopping(self):
+        finish = self.main_loop.log.current_row.get(
+            'training_finish_requested', False)
+        self.assertTrue(finish)
 
-    def check_stopping():
-        finish = main_loop.log.current_row.get('training_finish_requested',
-                                               False)
-        assert finish
+    def check_log(self, record_name, value):
+        self.assertEqual(self.main_loop.log.current_row[record_name], value)
 
-    ext = FinishIfNoImprovementAfter(3, 'bananas')
-    ext.main_loop = main_loop
-    # This should silently pass, since there has been no new best set,
-    # and 'bananas' is not in the log's current row.
-    ext.do('after_batch')
-    # First is a new best.
-    main_loop.log.current_row['bananas'] = True
-    main_loop.log.status['iterations_done'] += 1
-    ext.do('after_batch')
-    assert main_loop.log.current_row['bananas_patience'] == 3
-    check_not_stopping()
-    # No best found for another  2 iterations.
-    del main_loop.log.current_row['bananas']
-    main_loop.log.status['iterations_done'] += 1
-    ext.do('after_batch')
-    assert main_loop.log.current_row['bananas_patience'] == 2
-    check_not_stopping()
-    # One iteration down, one to go.
-    main_loop.log.status['iterations_done'] += 1
-    ext.do('after_batch')
-    assert main_loop.log.current_row['bananas_patience'] == 1
-    check_not_stopping()
-    # Oh look, a new best!
-    main_loop.log.current_row['bananas'] = True
-    main_loop.log.status['iterations_done'] += 1
-    ext.do('after_batch')
-    assert main_loop.log.current_row['bananas_patience'] == 3
-    check_not_stopping()
-    # Now, run out our patience. 3 iterations with no best.
-    del main_loop.log.current_row['bananas']
-    main_loop.log.status['iterations_done'] += 1
-    ext.do('after_batch')
-    assert main_loop.log.current_row['bananas_patience'] == 2
-    check_not_stopping()
-    main_loop.log.status['iterations_done'] += 1
-    ext.do('after_batch')
-    assert main_loop.log.current_row['bananas_patience'] == 1
-    check_not_stopping()
-    main_loop.log.status['iterations_done'] += 1
-    ext.do('after_batch')
-    assert main_loop.log.current_row['bananas_patience'] == 0
-    check_stopping()
+    def setUp(self):
+        self.main_loop = FakeMainLoop()
+
+    def check_finish_if_no_improvement_after(self, ext, epochs=False):
+        # ext = FinishIfNoImprovementAfter('bananas', iterations=3)
+        ext.main_loop = self.main_loop
+        # This should silently pass, since there has been no new best set,
+        # and 'bananas' is not in the log's current row.
+        which_callback = 'after_batch' if not epochs else 'after_epoch'
+        log_entry = 'bananas_patience_' + (
+            'iterations' if not epochs else 'epochs')
+        ext.do(which_callback)
+        # First is a new best.
+        self.main_loop.log.current_row['bananas'] = True
+        self.main_loop.log.advance(epochs)
+        ext.do(which_callback)
+        self.check_log(log_entry, 3)
+        self.check_not_stopping()
+        # No best found for another  2 iterations.
+        del self.main_loop.log.current_row['bananas']
+        self.main_loop.log.advance(epochs)
+        ext.do(which_callback)
+        self.check_log(log_entry, 2)
+        self.check_not_stopping()
+        # One iteration down, one to go.
+        self.main_loop.log.advance(epochs)
+        ext.do(which_callback)
+        self.check_log(log_entry, 1)
+        self.check_not_stopping()
+        # Oh look, a new best!
+        self.main_loop.log.current_row['bananas'] = True
+        self.main_loop.log.advance(epochs)
+        ext.do(which_callback)
+        self.check_log(log_entry, 3)
+        self.check_not_stopping()
+        # Now, run out our patience. 3 iterations with no best.
+        del self.main_loop.log.current_row['bananas']
+        self.main_loop.log.advance(epochs)
+        ext.do(which_callback)
+        self.check_log(log_entry, 2)
+        self.check_not_stopping()
+        self.main_loop.log.advance(epochs)
+        ext.do(which_callback)
+        self.check_log(log_entry, 1)
+        self.check_not_stopping()
+        self.main_loop.log.advance(epochs)
+        ext.do(which_callback)
+        self.check_log(log_entry, 0)
+        self.check_stopping()
+
+    def test_finish_if_no_improvement_after_iterations(self):
+        ext = FinishIfNoImprovementAfter('bananas', iterations=3)
+        self.check_finish_if_no_improvement_after(ext, epochs=False)
+
+    def test_finish_if_no_improvement_after_epochs(self):
+        ext = FinishIfNoImprovementAfter('bananas', epochs=3)
+        self.check_finish_if_no_improvement_after(ext, epochs=True)
+
+    def test_finish_if_no_improvement_after_specify_both(self):
+        self.assertRaises(ValueError, FinishIfNoImprovementAfter, 'boo',
+                          epochs=4, iterations=5)
