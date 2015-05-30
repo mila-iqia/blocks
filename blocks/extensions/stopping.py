7542754ffa671900efa0e00cc787f5cea2c7130d
David Warde-Farley <wardefar@iro.umontreal.ca>
2015-05-30 12:06:43 -0400

    Add patience logging.

--- blocks/extensions/stopping.py
+++ blocks/extensions/stopping.py
@@ -12,13 +12,22 @@ class FinishIfNoImprovementAfter(FinishAfter):
         The name of the log record to look for which indicates a new
         best performer has been found.  Note that the value of this
         record is not inspected.
+    patience_log_record : str, optional
+        The name under which to record the number of iterations we
+        are currently willing to wait for a new best performer.
+        Defaults to `notification_name + 'patience'`.
 
     """
-    def __init__(self, iterations, notification_name, **kwargs):
+    def __init__(self, iterations, notification_name, patience_log_record=None,
+                 **kwargs):
         self.iterations = iterations
         self.notification_name = notification_name
         kwargs.setdefault('after_batch', True)
         self.last_best = None
+        if patience_log_record is None:
+            self.patience_log_record = notification_name + '_patience'
+        else:
+            self.patience_log_record = patience_log_record
         super(FinishIfNoImprovementAfter, self).__init__(**kwargs)
 
     def update_best(self):
@@ -30,6 +39,8 @@ class FinishIfNoImprovementAfter(FinishAfter):
         self.update_best()
         iters_since = (self.main_loop.log.status['iterations_done'] -
                        self.last_best)
+        patience = self.iterations - iters_since
+        self.main_loop.log.current_row[self.patience_log_record] = patience
         if iters_since >= self.iterations:
             super(FinishIfNoImprovementAfter, self).do(which_callback,
                                                        *args)
--- tests/extensions/test_stopping.py
+++ tests/extensions/test_stopping.py
@@ -29,29 +29,36 @@ def test_finish_if_no_improvement_after():
     main_loop.log.current_row['bananas'] = True
     main_loop.log.status['iterations_done'] += 1
     ext.do('after_batch')
+    assert main_loop.log.current_row['bananas_patience'] == 3
     check_not_stopping()
     # No best found for another  2 iterations.
     del main_loop.log.current_row['bananas']
     main_loop.log.status['iterations_done'] += 1
     ext.do('after_batch')
+    assert main_loop.log.current_row['bananas_patience'] == 2
     check_not_stopping()
     # One iteration down, one to go.
     main_loop.log.status['iterations_done'] += 1
     ext.do('after_batch')
+    assert main_loop.log.current_row['bananas_patience'] == 1
     check_not_stopping()
     # Oh look, a new best!
     main_loop.log.current_row['bananas'] = True
     main_loop.log.status['iterations_done'] += 1
     ext.do('after_batch')
+    assert main_loop.log.current_row['bananas_patience'] == 3
     check_not_stopping()
     # Now, run out our patience. 3 iterations with no best.
     del main_loop.log.current_row['bananas']
     main_loop.log.status['iterations_done'] += 1
     ext.do('after_batch')
+    assert main_loop.log.current_row['bananas_patience'] == 2
     check_not_stopping()
     main_loop.log.status['iterations_done'] += 1
     ext.do('after_batch')
+    assert main_loop.log.current_row['bananas_patience'] == 1
     check_not_stopping()
     main_loop.log.status['iterations_done'] += 1
     ext.do('after_batch')
+    assert main_loop.log.current_row['bananas_patience'] == 0
     check_stopping()
