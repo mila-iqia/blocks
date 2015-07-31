1b23ac424a79fecfa96bffa5b9237faca5d80528
rizar <dimabv@tut.by>
2015-07-31 17:49:19 +0300

    As2D becomes WithHiddenDims<F2>

--- blocks/bricks/wrappers.py
+++ blocks/bricks/wrappers.py
@@ -2,40 +2,86 @@ from six.moves import xrange
 from theano import tensor
 
 from blocks.bricks.base import Brick, application
+from blocks.utils import dict_subset, pack
 
 
-class As2D(Brick):
-    """Wraps a 2D application around reshapes to accomodate N-D tensors.
+class WithExtraDims(Brick):
+    """Wraps an application method to handle inputs with extra dimensions.
 
-    The input is first flattened across all but its last axis, then the
-    application method is called on the flattened input, and finally
-    the output is reshaped so that it has the same number of dimensions
-    as the input.
+    A brick can be often reused even when data has more dimensions
+    than in the default setting. An example is a situation when one wants
+    to apply :meth:`~blocks.bricks.Softmax.categorical_cross_entropy`
+    to temporal data, that is when an additional 'time' axis is prepended
+    to its both `x` and `y` inputs.
+
+    This bricks takes care of reshapes required to use an application
+    method of a brick with such data by merging the extra dimensions
+    with the first non-extra one. Two key assumptions
+    are made: that all inputs and outputs have the same number of extra
+    dimensions and that these extra dimensions are equal throughout
+    all inputs and outputs. If in your case these assumptions are violated,
+    you have to wrap your application method in a custom reshaping code.
+
+    While this might be inconvinient, this brick does not try to guess
+    the number of extra dimensions, but demands it as an argument.
+    The considerations of simplicity and reliability motivated this design
+    choice. Upon availability in Blocks of a mechanism to request the
+    expected number of dimensions for an input of a brick, this can be
+    reconsidered.
 
     Parameters
     ----------
     application_method : callable
-        Some brick's application method. The application is expected to
-        have exactly one input and one output.
+        A brick's application method.
 
     """
     def __init__(self, application_method, **kwargs):
-        super(As2D, self).__init__(**kwargs)
+        super(WithExtraDims, self).__init__(**kwargs)
         self.application_method = application_method
         self.children = [self.application_method.brick]
 
-    @application(inputs=['input_'], outputs=['output'])
-    def apply(self, input_):
-        shape, ndim = input_.shape, input_.ndim
-        if ndim > 2:
-            input_ = input_.reshape((tensor.prod(shape[:-1]), shape[-1]))
-            output = self.application_method(input_)
-            output = output.reshape(
-                tensor.set_subtensor(shape[-1], output.shape[1]),
-                ndim=ndim)
-        else:
-            output = self.application_method(input_)
-        return output
+        self.apply.inputs = self.application_method.inputs
+        self.apply.outputs = self.application_method.outputs
+
+    @application
+    def apply(self, extra_ndim, *args, **kwargs):
+        """Wraps the applicationd method with reshapes.
+
+        Parameters
+        ----------
+        extra_ndim : int
+            The number of extra dimensions.
+
+        """
+        inputs = dict(zip(self.apply.inputs, args))
+        inputs.update(dict_subset(kwargs, self.apply.inputs,
+                                  must_have=False))
+        reshaped_inputs = inputs
+        # To prevent pollution of the computation graph with no-ops
+        if extra_ndim > 0:
+            for name, input_ in inputs.items():
+                shape, ndim = input_.shape, input_.ndim
+                # Remember extra_dims for reshaping the outputs correctly.
+                # Does not matter from which input, since we assume
+                # extra dimension match for all inputs.
+                extra_dims = shape[:extra_ndim]
+                new_first_dim = tensor.prod(shape[:extra_ndim + 1])
+                new_shape = tensor.join(
+                    0, new_first_dim[None], shape[extra_ndim + 1:])
+                reshaped_inputs[name] = input_.reshape(
+                    new_shape, ndim=ndim - extra_ndim)
+        outputs = self.application_method(**reshaped_inputs)
+        if extra_ndim == 0:
+            return outputs
+        reshaped_outputs = []
+        for output in pack(outputs):
+            shape, ndim = output.shape, output.ndim
+            new_shape = tensor.join(
+                0, extra_dims, (shape[0] / tensor.prod(extra_dims))[None],
+                shape[1:])
+            reshaped_outputs.append(
+                output.reshape(new_shape, ndim=ndim + extra_ndim))
+        return reshaped_outputs
 
 
 class WithAxesSwapped(Brick):
--- tests/bricks/test_wrappers.py
+++ tests/bricks/test_wrappers.py
@@ -4,41 +4,70 @@ from numpy.testing import assert_allclose
 from six.moves import cPickle
 from theano import tensor
 
-from blocks.bricks import Linear
-from blocks.bricks.wrappers import As2D, WithAxesSwapped
+from blocks.bricks import Linear, Softmax
+from blocks.bricks.wrappers import WithExtraDims, WithAxesSwapped
 from blocks.initialization import Constant
 
 
-def test_as2d_ndim_gt_2():
+def test_with_extra_dims_ndim_gt_2():
     X = tensor.tensor4('X')
     brick = Linear(input_dim=3, output_dim=4, weights_init=Constant(1),
                    biases_init=Constant(0))
-    wrapper = As2D(brick.apply)
+    wrapper = WithExtraDims(brick.apply)
     wrapper.initialize()
-    f = theano.function([X], wrapper.apply(X))
+    f = theano.function([X], wrapper.apply(2, X))
     assert_allclose(
         f(numpy.ones(shape=(2, 2, 2, 3), dtype=theano.config.floatX)),
         3 * numpy.ones(shape=(2, 2, 2, 4), dtype=theano.config.floatX))
 
 
-def test_as2d_ndim_leq_2():
+def test_with_extra_dims_ndim_leq_2():
     X = tensor.matrix('X')
     brick = Linear(input_dim=3, output_dim=4, weights_init=Constant(1),
                    biases_init=Constant(0))
-    wrapper = As2D(brick.apply)
+    wrapper = WithExtraDims(brick.apply)
     wrapper.initialize()
-    f = theano.function([X], wrapper.apply(X))
+    f = theano.function([X], wrapper.apply(0, X))
     assert_allclose(
         f(numpy.ones(shape=(2, 3), dtype=theano.config.floatX)),
         3 * numpy.ones(shape=(2, 4), dtype=theano.config.floatX))
 
 
-def test_as2d_is_serializable():
+def test_with_extra_dims_is_serializable():
     brick = Linear(input_dim=3, output_dim=4, weights_init=Constant(1),
                    biases_init=Constant(0))
-    wrapper = As2D(brick.apply)
+    wrapper = WithExtraDims(brick.apply)
     wrapper.initialize()
     cPickle.loads(cPickle.dumps(wrapper))
+
+
+def test_with_extra_dims_cross_entropy_2d():
+    x = tensor.matrix('x')
+    y = tensor.lvector('y')
+    brick = Softmax()
+    wrapper = WithExtraDims(brick.categorical_cross_entropy)
+    f = theano.function([y, x], [wrapper.apply(0, y, x)])
+    assert_allclose(
+        f([0, 1, 2, 3],
+           [[1, 2, 1, 2], [1, 2, 3, 4],
+            [4, 3, 2, 1], [2, 2, 2, 2]])[0],
+        numpy.array([ 2.00640,  2.44019 ,  2.44019 ,  1.3863]),
+        rtol=1e-5)
+
+
+def test_with_extra_dims_cross_entropy_3d():
+    x = tensor.tensor3('x')
+    y = tensor.lmatrix('y')
+    brick = Softmax()
+    wrapper = WithExtraDims(brick.categorical_cross_entropy)
+    f = theano.function([y, x], [wrapper.apply(1, y, x)])
+    assert_allclose(
+        f([[0, 1], [2, 3]],
+          [[[1, 2, 1, 2], [1, 2, 3, 4]],
+           [[4, 3, 2, 1], [2, 2, 2, 2]]])[0],
+        numpy.array([[2.0064,  2.44019 ],
+               [2.44019 ,  1.3863]]),
+        rtol=1e-5)
 
 
 def test_withaxesswapped_dim0_dim1_neq():
