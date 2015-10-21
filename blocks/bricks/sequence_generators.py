6ebcb37e733c06b4df5b941414093d8bef879401
Jose Manuel <rdz.sotelo@gmail.com>
2015-10-21 17:18:21 -0400

    Added documentation about the emitter cost in sequence generators.

--- blocks/bricks/sequence_generators.py
+++ blocks/bricks/sequence_generators.py
@@ -582,6 +582,18 @@ class AbstractEmitter(Brick):
 
     :class:`SoftmaxEmitter` : for integer outputs
 
+    Notes
+    -----
+    An important detail about the emitter cost is that it will be evaluated
+    with inputs of different dimensions so it has to be flexible enough to
+    handle this. The two ways in which it can be applied are:
+
+        1. Inside the cost_matrix function of :class:`BaseSequenceGenerator`
+        where it will be applied to the whole sequence at once.
+        
+        2. Inside the generate function of :class:`BaseSequenceGenerator`
+        where it will be only applied to one step ot the sequence.
+
     """
     @abstractmethod
     def emit(self, readouts):
