679b01c2502c9ca9a7be03e72eae3df04610787e
Jose Manuel <rdz.sotelo@gmail.com>
2015-10-21 18:08:46 -0400

    Improved the error message when an application decorator is missing.

--- blocks/bricks/base.py
+++ blocks/bricks/base.py
@@ -296,7 +296,9 @@ class Application(object):
                 brick not in last_brick.children):
             raise ValueError('Brick ' + str(self.call_stack[-1]) + ' tries '
                              'to call brick ' + str(self.brick) + ' which '
-                             'is not in the list of its children.')
+                             'is not in the list of its children. This could '
+                             'be caused because an @application decorator is '
+                             'missing.')
         self.call_stack.append(brick)
         try:
             outputs = self.application_function(brick, *args, **kwargs)
