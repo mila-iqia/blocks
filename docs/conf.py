caf71be1107831321de2c73fe19d1451f535f610
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2016-04-07 18:54:26 -0400

    Put formatting inside url

--- docs/conf.py
+++ docs/conf.py
@@ -348,6 +348,6 @@ def linkcode_resolve(domain, info):
 
     fn = os.path.relpath(fn, start=os.path.dirname(blocks.__file__))
 
-    github = "https://github.com/mila-udem/blocks/blob/master/blocks/"
-    return github + "{}{}".format(fn, linespec)
+    github = "https://github.com/mila-udem/blocks/blob/master/blocks/{}{}"
+    return github.format(fn, linespec)
 
