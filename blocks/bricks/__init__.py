643b8adc9968b42c65ec4bbc06c60df23e57a2f6
Isac Arnekvist <isacar@kth.se>
2016-04-19 21:21:40 +0200

    export name leakyrectifier

--- blocks/bricks/__init__.py
+++ blocks/bricks/__init__.py
@@ -14,6 +14,6 @@ __all__ = ('application', 'Brick', 'lazy', 'BatchNormalization',
            'SpatialBatchNormalization', 'BatchNormalizedMLP',
            'Activation', 'Feedforward', 'Initializable', 'LinearLike',
            'Random', 'Linear', 'Bias', 'Maxout', 'LinearMaxout', 'Identity',
-           'Tanh', 'Logistic', 'Softplus', 'Rectifier', 'Softmax',
-           'NDimensionalSoftmax', 'Sequence', 'FeedforwardSequence',
-           'MLP', 'WithExtraDims')
+           'Tanh', 'Logistic', 'Softplus', 'Rectifier', 'LeakyRectifier',
+           'Softmax', 'NDimensionalSoftmax', 'Sequence',
+           'FeedforwardSequence', 'MLP', 'WithExtraDims')
