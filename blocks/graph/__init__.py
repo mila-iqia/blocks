78d12353ea20261ce7ff22094077d77a2bc061ff
rizar <dimabv@tut.by>
2016-05-30 10:53:30 -0400

    Compare scan Ops by their ids

--- blocks/graph/__init__.py
+++ blocks/graph/__init__.py
@@ -125,7 +125,8 @@ class ComputationGraph(object):
             inputs = graph.inputs(self.outputs)
             sorted_apply_nodes = graph.io_toposort(inputs, usual_outputs)
             self.scans = list(unique([node.op for node in sorted_apply_nodes
-                                     if isinstance(node.op, Scan)]))
+                                     if isinstance(node.op, Scan)],
+                                     key=lambda op: id(op)))
             self._scan_graphs = [ComputationGraph(scan.outputs)
                                  for scan in self.scans]
 
