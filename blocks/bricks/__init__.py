d4d886c15b15220935dfb326637fff72591cd851
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2016-06-01 18:29:21 -0400

    Add recurrent declarations to bricks.__init__

--- blocks/bricks/__init__.py
+++ blocks/bricks/__init__.py
@@ -4,6 +4,9 @@ from .bn import (BatchNormalization, SpatialBatchNormalization,
                  BatchNormalizedMLP)
 from .interfaces import (Activation, Feedforward, Initializable, LinearLike,
                          Random)
+from .recurrent import (BaseRecurrent, SimpleRecurrent, LSTM, GatedRecurrent,
+                        Bidirectional, RecurrentStack, RECURRENTSTACK_SEPARATOR,
+                        recurrent)
 from .simple import (Linear, Bias, Maxout, LinearMaxout, Identity, Tanh,
                      Logistic, Softplus, Rectifier, LeakyRectifier,
                      Softmax, NDimensionalSoftmax)
@@ -16,4 +19,7 @@ __all__ = ('application', 'Brick', 'lazy', 'BatchNormalization',
            'Random', 'Linear', 'Bias', 'Maxout', 'LinearMaxout', 'Identity',
            'Tanh', 'Logistic', 'Softplus', 'Rectifier', 'LeakyRectifier',
            'Softmax', 'NDimensionalSoftmax', 'Sequence',
-           'FeedforwardSequence', 'MLP', 'WithExtraDims')
+           'FeedforwardSequence', 'MLP', 'WithExtraDims',
+           'BaseRecurrent', 'SimpleRecurrent', 'LSTM', 'GatedRecurrent',
+           'Bidirectional', 'RecurrentStack', 'RECURRENTSTACK_SEPARATOR',
+           'recurrent')
